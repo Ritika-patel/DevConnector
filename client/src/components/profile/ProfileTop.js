1cfe221773f099fc220b793f032bb25ac44ff7ef
Ritika-patel <72975134+Ritika-patel@users.noreply.github.com>
2021-07-23 13:10:18 +0530

    'ok'

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -21,32 +21,32 @@ const ProfileTop = ({
       <div className='icons my-1'>
         {website && (
           <a href={website} target='_blank' rel='noopener noreferrer'>
-            <i className='fa fa-globe fa-2x' />
+          <i className="fas fa-globe fa-2x"></i>
           </a>
         )}
         {social && social.twitter && (
           <a href={social.twitter} target='_blank' rel='noopener noreferrer'>
-            <i className='fa fa-twitter fa-2x' />
+            <i className='fab fa-twitter fa-2x' />
           </a>
         )}
         {social && social.facebook && (
           <a href={social.facebook} target='_blank' rel='noopener noreferrer'>
-            <i className='fa fa-facebook fa-2x' />
+            <i className='fab fa-facebook fa-2x' />
           </a>
         )}
         {social && social.linkedin && (
           <a href={social.linkedin} target='_blank' rel='noopener noreferrer'>
-            <i className='fa fa-linkedin fa-2x' />
+          <i className="fab fa-linkedin fa-2x"></i>
           </a>
         )}
         {social && social.youtube && (
           <a href={social.youtube} target='_blank' rel='noopener noreferrer'>
-            <i className='fa fa-youtube fa-2x' />
+            <i className='fab fa-youtube fa-2x' />
           </a>
         )}
         {social && social.instagram && (
           <a href={social.instagram} target='_blank' rel='noopener noreferrer'>
-            <i className='fa fa-instagram fa-2x' />
+            <i className='fab fa-instagram fa-2x' />
           </a>
         )}
       </div>
