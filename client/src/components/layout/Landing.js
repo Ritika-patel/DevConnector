ed2603c3242e86bb50d5deddb1426c1e98454f5e
Ritika-patel <72975134+Ritika-patel@users.noreply.github.com>
2021-08-16 02:21:51 +0530

    css-updated-2

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -19,10 +19,10 @@ const Landing = ({ isAuthenticated }) => {
             other developers
           </p>
           <div className='buttons'>
-            <Link to='/register' className='btn btn-primary'>
+            <Link to='/register' className='btn-landing btn-primary'>
               Sign Up
             </Link>
-            <Link to='/login' className='btn btn-light'>
+            <Link to='/login' className='btn-landing btn-light'>
               Login
             </Link>
           </div>
